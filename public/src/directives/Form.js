b9a5066d4726cc145dbfbfb68dbda1d610b9c2db
arun nampoothiri <arun.vishnu.n@gmail.com>
2017-02-06 08:21:42 -0800

    Add unit timestamp validation

--- public/src/directives/Form.js
+++ public/src/directives/Form.js
@@ -6,7 +6,7 @@ app.directive('jsonSchemaForm',  ['JsonService', function Form(JsonService) {
     templateUrl: 'src/directives/form.html',
     controllerAs: 'form',
     link: function(scope, element, attr) {
-
+      // Get schema to build the form.
       JsonService.then(function(json) {
         scope.schema = json.data['properties'];
       });
@@ -20,7 +20,30 @@ app.directive('jsonSchemaForm',  ['JsonService', function Form(JsonService) {
         var typeToCompare = scope.isPrimitive(item) ? item.type : item.items.type;
         return (typeToCompare === 'string') ? 'text' : 'number';
       }
-
     }
   };
 }]);
+
+/**
+ * Validation for Unix epoch time.
+ * Usage: add epochvalidate as attibute to dom element
+ */
+app.directive('epochvalidate', function () {
+  return {
+    restrict: 'A',
+     require: 'ngModel',
+    link: function (scope, element, attrs, ctrl) {
+      ctrl.$parsers.unshift(function (viewValue) {
+        var UNIX_TIME_REGEXP = /^\d{10}$|^\d{13}$/;
+        // If valid return true
+        if (UNIX_TIME_REGEXP.test(viewValue.toString())) {
+          ctrl.$setValidity('epochvalidate', true);
+          return viewValue;
+        } else { // it is invalid, return undefined (no model update)
+          ctrl.$setValidity('epochvalidate', false);
+          return undefined;
+        }
+      });
+    }
+  };
+});
