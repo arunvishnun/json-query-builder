676eae3a3a4da9c89ae6022c321ce289c7260d02
Arun Nampoothiri <arun.vishnu.n@gmail.com>
2017-02-06 12:18:07 -0800

    Update app.js
    minor clean up to keep single url and remove unused template

--- public/src/app.js
+++ public/src/app.js
@@ -1,12 +1,15 @@
 var queryBuilderApp = angular.module('queryBuilderApp',['ngRoute']);
 
-queryBuilderApp.config(function($routeProvider) {
+queryBuilderApp.config(function($routeProvider, $locationProvider) {
 	'use strict';
 
-	$routeProvider.when('/form', {
-		templateUrl: 'views/form.html',
+	$routeProvider.when('/', {
 		controller: 'FormController',
 		controllerAs: 'form'
-	});
+		}).otherwise({
+      			redirectTo: '/'
+  		});
 
+	// Just to make clean URL
+  	$locationProvider.html5Mode(true);
 });
